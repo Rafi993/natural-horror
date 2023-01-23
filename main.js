c659bf45d111773ab616dab8aa44ce6ccf075e1f
rafi993 <rafi993@protonmail.com>
2023-01-23 12:26:29 +0530

    Removing un-used code

--- main.js
+++ main.js
@@ -3,41 +3,20 @@ import ColorHash from 'color-hash';
 
 import './style.css';
 
-const djb2 = (str) => {
-  let hash = 5381;
-  for (let i = 0; i < str.length; i++) {
-    hash = (hash << 5) + hash + str.charCodeAt(i);
-  }
-  return hash;
-};
-
-const hashStringToColor = (str) => {
-  const hash = djb2(str);
-  const r = (hash & 0xff0000) >> 16;
-  const g = (hash & 0x00ff00) >> 8;
-  const b = hash & 0x0000ff;
-  return (
-    '#' +
-    ('0' + r.toString(16)).substr(-2) +
-    ('0' + g.toString(16)).substr(-2) +
-    ('0' + b.toString(16)).substr(-2)
-  );
-};
-
 d3.json('/data.json', (err, rows) => {
-  const colorHash = new ColorHash();
+  const colorHash = new ColorHash({ lightness: 0.5 });
 
   const unpack = (categoryData, key) => categoryData.map((row) => row[key]);
   const getColor = (categoryData) =>
     categoryData.map((row) => colorHash.hex(row.Category));
   const getTooltip = (categoryData) =>
-    categoryData.map(
-      (row) =>
-        `<b>Category:</b> ${row.Category} <br>` +
+    categoryData.map((row) => [
+      `<b>Category:</b> ${row.Category} <br>` +
         `<b>Animal:</b> ${row.Animal} <br>` +
         `<b>Movie:</b> ${row.Film} (${row.Year}) <br>` +
-        `<b>Location:</b> ${row.Location}`,
-    );
+        `<b>Location:</b> ${row.Location} <br>`,
+      row.Link,
+    ]);
 
   const data = Object.values(rows).map((categoryData) => {
     return {
@@ -46,10 +25,11 @@ d3.json('/data.json', (err, rows) => {
       text: getTooltip(categoryData),
       lon: unpack(categoryData, 'Long'),
       lat: unpack(categoryData, 'Lat'),
-      hovertemplate: `%{text}<extra></extra>`,
-      marker: { color: getColor(categoryData), size: 8 },
+      hovertemplate: `%{text[0]}<extra></extra>`,
+      marker: { color: getColor(categoryData), size: 8, cursor: 'pointer' },
       name: categoryData[0].Category,
       showlegend: true,
+      link: unpack(categoryData, 'Link'),
     };
   });
 
@@ -73,5 +53,13 @@ d3.json('/data.json', (err, rows) => {
     config: { responsive: true },
   };
 
+  const mapPlot = document.getElementById('app');
   Plotly.newPlot('app', data, layout);
+  mapPlot.on('plotly_click', (data) => {
+    const link = data.points[0].data.link[0];
+
+    if (link) {
+      window.open(link, '_blank');
+    }
+  });
 });
