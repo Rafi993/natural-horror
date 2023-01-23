b78611675119125cc2c7abbf19af0ea5a15127dc
rafi993 <rafi993@protonmail.com>
2023-01-23 18:38:27 +0530

    Adding search

--- main.js
+++ main.js
@@ -3,8 +3,53 @@ import ColorHash from 'color-hash';
 
 import './style.css';
 
+const renderSearch = (result) => {
+  const mapPlot = document.getElementById('map');
+  mapPlot.style.display = 'none';
+  const searchResult = document.getElementById('searchResult');
+  searchResult.style.display = 'block';
+
+  for (const item of result) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+     <div>
+        <a href="${item.Link}" target="_blank">${item.Film}(${item.Year})</a>
+        <tag>${item.Category}</tag>
+     </div>
+    `;
+
+    searchResult.appendChild(li);
+  }
+};
+
+const hideSearch = () => {
+  const mapPlot = document.getElementById('map');
+  mapPlot.style.display = 'block';
+  const searchResult = document.getElementById('searchResult');
+  searchResult.style.display = 'none';
+  searchResult.replaceChildren();
+};
+
 d3.json('/data.json', (err, rows) => {
-  const colorHash = new ColorHash({ lightness: 0.5 });
+  const colorHash = new ColorHash({ lightness: 0.5, saturation: 0.6 });
+
+  const search = document.getElementById('search');
+  search.addEventListener('keyup', (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+
+    if (searchTerm.length === 0) {
+      hideSearch();
+      return;
+    }
+
+    const result = Object.values(rows)
+      .reduce((acc, item) => [...acc, ...item], [])
+      .filter((item) =>
+        item.Film.toLowerCase().includes(searchTerm.toLowerCase()),
+      );
+
+    renderSearch(result);
+  });
 
   const unpack = (categoryData, key) => categoryData.map((row) => row[key]);
   const getColor = (categoryData) =>
@@ -27,7 +72,7 @@ d3.json('/data.json', (err, rows) => {
       lat: unpack(categoryData, 'Lat'),
       hovertemplate: `%{text[0]}<extra></extra>`,
       marker: { color: getColor(categoryData), size: 8, cursor: 'pointer' },
-      name: categoryData[0].Category,
+      name: `${categoryData[0].Category} (${categoryData.length})`,
       showlegend: true,
       link: unpack(categoryData, 'Link'),
     };
@@ -53,8 +98,8 @@ d3.json('/data.json', (err, rows) => {
     config: { responsive: true },
   };
 
-  const mapPlot = document.getElementById('app');
-  Plotly.newPlot('app', data, layout);
+  const mapPlot = document.getElementById('map');
+  Plotly.newPlot('map', data, layout);
   mapPlot.on('plotly_click', (data) => {
     const link = data.points[0].data.link[0];
 
